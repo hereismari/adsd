824b05f5921963b7adc78b4736ef76115f3c400a
Marianne Linhares <mariannelinharesm@gmail.com>
2019-05-07 01:48:12 +0100

    Change test order

--- mt_simulacao/test/test_event.py
+++ mt_simulacao/test/test_event.py
@@ -7,8 +7,8 @@ def test_comparisson():
     event1 = Event(event_type=EventType.CHEGADA_1, name='event1')
     event2 = Event(event_type=EventType.CHEGADA_2, name='event2')
     event3 = Event(event_type=EventType.FIM_DE_SERVICO, name='event3')
-    event4 = Event(event_type=EventType.FIM_DE_SERVICO, name='event4')
     event5 = Event(event_type=EventType.CHEGADA_2, name='event5')
+    event4 = Event(event_type=EventType.FIM_DE_SERVICO, name='event4')
     event6 = Event(event_type=EventType.CHEGADA_1, name='event6')
 
     events = [event1, event2, event3, event4, event5, event6]
