d76ef129d0952507b73d7741d7aa99ae2620376b
Marianne Linhares <mariannelinharesm@gmail.com>
2019-05-09 02:18:14 +0100

    Add UniformDistributionMCGBucket

--- mt_simulacao/src/rng.py
+++ mt_simulacao/src/rng.py
@@ -32,13 +32,17 @@ class MixedCongruentialGenerator(RandomNumberGenerator):
         is 1
         2. If q is a prime number that divides mod, then q divides multi_factor-1
         3. If 4 divides mod, then 4 divides multi_factor-1 
+
+    Default values from:
+        http://www.columbia.edu/~ks20/4404-16-Fall/Simulation-LCG.pdf
     """
-    def __init__(self, multi_factor: int = 1103515245,
-                 add_factor: int = 12345, mod: int = 8765, seed=333):
+    def __init__(self, multi_factor: int = 1664525,
+                 add_factor: int = 1013904223, mod: int = int(2e32),
+                 seed=333):
         super().__init__(seed)
         self.multi_factor = multi_factor
         self.add_factor = add_factor
-        self.mod = mod
+        self.mod = int(mod)
         self.previous_value = seed
 
     def sample(self):
@@ -48,7 +52,12 @@ class MixedCongruentialGenerator(RandomNumberGenerator):
 
 
 class UniformDistributionMCG(RandomNumberGenerator):
-    """Uniform distribution random number generator."""
+    """Uniform distribution random number generator.
+    
+    Mod = upper_bound - lower_bound + 1. Not actual
+    random distribution since after a cycle the values just
+    repeat itself.
+    """
 
     def __init__(self, lower_bound: int, upper_bound: int, **kwargs):
         super().__init__(self)
@@ -60,6 +69,34 @@ class UniformDistributionMCG(RandomNumberGenerator):
 
     def sample(self):
         return self.mcg.sample() + self.lower_bound
+
+
+class UniformDistributionMCGBucket(RandomNumberGenerator):
+    """Uniform distribution random number generator.
+    
+    Using a MCG with a high value mod and buckets to generate an uniform
+    distribution.
+    """
+
+    def __init__(self, lower_bound: int, upper_bound: int, **kwargs):
+        super().__init__(self)
+        
+        self.mcg = MixedCongruentialGenerator(**kwargs)
+        
+        self._intervals = {}
+        bucket_start, bucket_size = 0, self.mcg.mod // (upper_bound - lower_bound + 1)
+        for value in range(lower_bound, upper_bound + 1):
+            self._intervals[value] = (bucket_start, bucket_start + bucket_size)
+            _, bucket_start = self._intervals[value]
+        
+    def sample(self):
+        random_value = self.mcg.sample()
+        for value in self._intervals:
+            bucket_start, bucket_end = self._intervals[value]
+            if random_value >= bucket_start and random_value < bucket_end:
+                return value
+        
+        return self.sample()
 
 
 class UniformDistribution(RandomNumberGenerator):
